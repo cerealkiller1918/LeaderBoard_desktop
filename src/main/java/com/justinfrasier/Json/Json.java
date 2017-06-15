ed724d8ecfe1d0303421a959c51acc32a1a5738d
Justin Frasier <Justin_Frasier@me.com>
2017-06-15 11:36:17 -0500

    Added all the json api and all the test cases

--- src/main/java/com/justinfrasier/Json/Json.java
+++ src/main/java/com/justinfrasier/Json/Json.java
@@ -1,21 +1,76 @@
 package com.justinfrasier.Json;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.justinfrasier.https.HTTPS;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Json {
 
     private JsonObject json;
 
-    public void getJsonFile(){
+    public Json() {
         String url = "https://www.fantasyfootballnerd.com/service/schedule/json/yhnkisjxcbsm/";
         String s = new HTTPS().sendGet(url);
-        json = new Gson().fromJson(s,JsonObject.class);
+        json = new Gson().fromJson(s, JsonObject.class);
     }
 
-    public int currentWeek(){
+    public JsonArray getScheduleArray() {
+        return json.get("Schedule").getAsJsonArray();
+    }
+
+    public String getData(int gameId, String data){
+        JsonArray array = getScheduleArray();
+        for (JsonElement j : array) {
+            JsonObject k = (JsonObject) j;
+            if (gameId == k.get("gameId").getAsInt())
+                return k.get(data).getAsString();
+        }
+        return null;
+    }
+
+    public int currentWeek() {
         String output = json.get("currentWeek").getAsString();
         return Integer.parseInt(output);
     }
+
+    public List<Integer> gamesInCurrentWeek(int currentWeek) {
+        List<Integer> games = new ArrayList<Integer>();
+        JsonArray array = getScheduleArray();
+        for (int i = 0; i < array.size(); i++) {
+            JsonObject j = (JsonObject) array.get(i);
+            int gameWeek = j.get("gameWeek").getAsInt();
+            if (gameWeek == currentWeek) {
+                games.add(j.get("gameId").getAsInt());
+            }
+        }
+        return games;
+    }
+
+    public int getGameWeek(int gameId) {
+        JsonArray array = getScheduleArray();
+        for (JsonElement j : array) {
+            JsonObject k = (JsonObject) j;
+            if (gameId == k.get("gameId").getAsInt())
+                return k.get("gameWeek").getAsInt();
+        }
+        return -1;
+    }
+
+    public String getGameDate(int gameId) {return getData(gameId, "gameDate");}
+
+    public String getAwayTeam(int gameId) {return getData(gameId, "awayTeam");}
+
+    public String getHomeTeam(int gameId) {return getData(gameId, "homeTeam");}
+
+    public String getGameTimeET(int gameId){ return getData(gameId, "gameTimeET");}
+
+    public String getTvStation(int gameId){return getData(gameId, "tvStation");}
+
+    public String getWinner(int gameId){return getData(gameId,"winner");}
+
 }
--- src/test/java/com/justinfrasier/test/JsonTest.java
+++ src/test/java/com/justinfrasier/test/JsonTest.java
@@ -1,16 +1,81 @@
 package com.justinfrasier.test;
 
 import com.justinfrasier.Json.Json;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class JsonTest {
 
+    Json json;
+
+    @Before
+    public void before(){
+      json = new Json();
+    }
+
     @Test
-    public void currentWeek() throws Exception{
-        Json json = new Json();
-        json.getJsonFile();
+    public void currentWeekTest() throws Exception{
         assertEquals(1,json.currentWeek());
     }
+
+    @Test
+    public void gamesInCurrentWeekTest(){
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i =1; i<17 ; i++) list.add(i);
+        assertEquals(list,json.gamesInCurrentWeek(1));
+    }
+
+    @Test
+    public void getGameWeekTest(){
+        assertEquals(1, json.getGameWeek(1));
+    }
+
+    @Test
+    public void getGameWeekBreakTest(){
+        assertEquals(-1, json.getGameWeek(257));
+    }
+
+    @Test
+    public void getGameDateTest1(){
+        assertEquals("2017-09-07", json.getGameDate(1));
+    }
+
+    @Test
+    public void getGameDateTest2(){
+        assertEquals("2017-10-01", json.getGameDate(56));
+    }
+
+    @Test
+    public void getGameDateTest3(){
+        assertEquals("2017-09-17", json.getGameDate(30));
+    }
+
+    @Test
+    public void getGameDateTestBreak(){
+        assertEquals(null, json.getGameDate(257));
+    }
+
+    @Test
+    public void getAwayTeamTest(){
+        assertEquals("DET", json.getAwayTeam(56));
+    }
+
+    @Test
+    public void getHomeTeamTest(){
+        assertEquals("MIN", json.getHomeTeam(56));
+    }
+
+    @Test
+    public void getGameTimeTest(){assertEquals("1:00 PM", json.getGameTimeET(56));}
+
+    @Test
+    public void getTvStationTest(){assertEquals("FOX", json.getTvStation(56));}
+
+    @Test
+    public void getWinnerTest(){assertEquals("", json.getWinner(1));}
 }
